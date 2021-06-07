03d29fb3625ddb7256b81f206fa07b6d9afebd75
Vev <justincastro105@gmail.com>
2021-06-07 13:57:04 +0800

    Update people in DB

--- server/index.js
+++ server/index.js
@@ -29,10 +29,25 @@ app.get('/viewPeople', (req,res) => {//get all people's name from db table
 app.post('/insertPeople', (req,res) => {//Insert People to DB table
 
     const name = req.body.name;
-    console.log("SAMPLEOUTPUTSSSS", name);
     db.query(
         "INSERT INTO simplecrudtable(pName) VALUES(?)",
         [name],
+        (err, result) => {
+            if(err){
+                console.log(err);
+            }else{
+                res.send("Success!");
+            }
+        });
+});
+
+app.post('/updatePeople', (req,res) => {//Update People to DB table
+
+    const name = req.body.name;
+    const id = req.body.id;
+    db.query(
+        "UPDATE simplecrudtable SET pName=? WHERE pId =?",
+        [name,id],
         (err, result) => {
             if(err){
                 console.log(err);
